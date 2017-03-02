5e175445fe075aca26a75f91b9145a0df2333422
Xu Li <Madman@XudeMacBook-Pro.local>
2017-03-02 18:04:35 +0100

    Folder added

---
+++ src/tool/MainApp.java
@@ -0,0 +1,18 @@
+package tool;/**
+ * Created by Madman on 02.03.17.
+ */
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class MainApp extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
